0893d7bc767139ee1d178f61026c6f0417a9577f
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-01-07 16:11:57 +0200

    Define a function to play one round

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 // Get the input from the computer by random method and store it in the variable
 function getComputerChoice(){
-  // define an array to store string values of choices
+  // Define an array to store string values of choices
   const gameWords = ['rock', 'paper', 'scissors']
-  //define a variable to store random choice
+  // Define a variable to store random choice
   let computerInput = gameWords[Math.floor(Math.random()*3)]
   console.log(computerInput);
-  // return a random choice
+  // Return a random choice
   return computerInput;
 }
 
@@ -15,3 +15,47 @@ function getPlayerChoice(){
   let choice = prompt("Enter paper, rock or scissors");
   return choice;
 }
+
+// Define a function for playing one round
+function playRound(playerSelection, computerSelection){
+  // Make sure the plaerSelection parameter is case-insensitive
+  playerSelection = playerSelection.toLowerCase();
+  console.log(playerSelection)
+  // Make a condition to check if the player wins or loses against the computer
+  switch(playerSelection){
+    // If the selection is rock
+    case 'rock':
+      if(computerSelection === 'paper'){
+        return "You Lose! Paper beats Rock";
+      } 
+      else if(computerSelection === 'scissors'){
+        return "You Win! Rock beats Scissors";
+      }else{
+        return "Draw";
+      }
+    // If the selection is paper  
+    case 'paper':
+      if(computerSelection === 'rock'){
+        return "You Win! Paper beats Rock";
+      } 
+      else if(computerSelection === 'scissors'){
+        return "You Lose! Scissors beats Paper";
+      }else{
+        return "Draw";
+      }
+    // If the selection is scissors  
+    case 'scissors':
+      if(computerSelection === 'rock'){
+        return "You Lose! Rock beats Scissors";
+      } 
+      else if(computerSelection === 'paper'){
+        return "You Win! Scissors beats Paper";
+      }else{
+        return "Draw";
+      }
+    // If the selection is not defined 
+    default:
+      return "You must enter paper, rock or scissors"
+  }
+}
+
