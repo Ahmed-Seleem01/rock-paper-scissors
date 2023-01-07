05ab7ba7c31e74ec1d58acb7f1e9d39ace2e5182
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-01-07 16:03:45 +0200

    Get player choice

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Get the input from the computer by random method and store it in the variable
 function getComputerChoice(){
   // define an array to store string values of choices
-  const gameWords = ['Rock', 'Paper', 'Scissors']
+  const gameWords = ['rock', 'paper', 'scissors']
   //define a variable to store random choice
   let computerInput = gameWords[Math.floor(Math.random()*3)]
   console.log(computerInput);
@@ -11,3 +11,7 @@ function getComputerChoice(){
 
 
 // Get the input form the player by prompt method and store it in the variable
+function getPlayerChoice(){
+  let choice = prompt("Enter paper, rock or scissors");
+  return choice;
+}
