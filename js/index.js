807ec2d33e3a41314fd1ba309cda1223682eb807
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-01-11 22:22:13 +0200

    Add little UI to the game

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 // Get the input from the computer by random method and store it in the variable
 function getComputerChoice(){
   // Define an array to store string values of choices
-  const gameWords = ['rock', 'paper', 'scissors']
+  const gameWords = ['rock', 'paper', 'scissors'];
   // Define a variable to store random choice
-  let computerInput = gameWords[Math.floor(Math.random()*3)]
+  let computerInput = gameWords[Math.floor(Math.random()*3)];
   console.log(computerInput);
   // Return a random choice
   return computerInput;
@@ -11,15 +11,24 @@ function getComputerChoice(){
 
 
 // Get the input form the player by prompt method and store it in the variable
-function getPlayerChoice(e){
-  let choice = 'e.target.textContent';
-  console.log(this);
-  return choice;
-}
+// Define variables to track the win count 
+let playerWinCount = 0;
+let computerWinCount = 0;
+const resultDiv = document.querySelector('.result');
 const buttons = document.querySelectorAll('button');
-console.log(buttons)
+
 buttons.forEach(btn => {
-  btn.addEventListener('click', getPlayerChoice);
+  btn.addEventListener('click', (e)=>{
+    playGame(e);
+    if(playerWinCount ===5){
+      resultDiv.innerText = 'You Win!';
+      playerWinCount = 0;
+    }
+    else if(computerWinCount === 5){
+      resultDiv.innerText = 'You Lose!';
+      computerWinCount = 0;
+    }
+  });
 });
 // Define a function for playing one round
 function playRound(playerSelection, computerSelection){
@@ -64,32 +73,21 @@ function playRound(playerSelection, computerSelection){
   }
 }
 
-// Define a function to play the game and it is IIFE (immediately invoked function expression)
-function game(){
-  // Define variables to track the win count 
-  let playerWinCount = 0;
-  let computerWinCount = 0;
-  // Make a loop to play five times
-  for (let i =0; i< 5; i++){
-    let result = playRound(getPlayerChoice(), getComputerChoice());
-    console.log(result);
-    // If the player wins increase his win count
-    if(result.includes("You Win")){
-      playerWinCount++;
-    }
-    // If the computer wins increase its win count
-    else if(result.includes("You Lose")){
-      computerWinCount++;
-    }
+// Define a function to play the game
+function playGame(e){
+  
+  let result = playRound(e.target.textContent, getComputerChoice());
+  
+  // If the player wins increase his win count
+  if(result.includes("You Win")){
+    playerWinCount++;
   }
-  // Make a check if the player wins
-  if(playerWinCount > computerWinCount){
-    console.log("You Win the game")
-  }
-  // Make a check if the computer wins
-  else if(playerWinCount < computerWinCount){
-    console.log("You Lose the game")
-  }else{
-    console.log("Draw")
-  }
+  // If the computer wins increase its win count
+  else if(result.includes("You Lose")){
+    computerWinCount++;
+  }  
+
+  resultDiv.innerText = `${result}
+  Player win count: ${playerWinCount}
+  Computer win count: ${computerWinCount}`;
 };
