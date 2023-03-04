cab8d2223585b529342e09d90c363e2792be9f4d
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-03-04 09:55:43 +0200

    Apply prettier to fix the code style

--- js/index.js
+++ js/index.js
@@ -1,95 +1,89 @@
 // Get the input from the computer by random method and store it in the variable
-function getComputerChoice(){
+function getComputerChoice() {
   // Define an array to store string values of choices
-  const gameWords = ['rock', 'paper', 'scissors'];
+  const gameWords = ["rock", "paper", "scissors"];
   // Define a variable to store random choice
-  let computerInput = gameWords[Math.floor(Math.random()*3)];
+  let computerInput = gameWords[Math.floor(Math.random() * 3)];
   console.log(computerInput);
   // Return a random choice
   return computerInput;
 }
 
-
 // Get the input form the player by prompt method and store it in the variable
-// Define variables to track the win count 
+// Define variables to track the win count
 let playerWinCount = 0;
 let computerWinCount = 0;
-const resultDiv = document.querySelector('.result');
-const buttons = document.querySelectorAll('button');
+const resultDiv = document.querySelector(".result");
+const buttons = document.querySelectorAll("button");
 
-buttons.forEach(btn => {
-  btn.addEventListener('click', (e)=>{
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
     playGame(e);
-    if(playerWinCount ===5){
-      resultDiv.innerText = 'Final result: You Win!';
+    if (playerWinCount === 5) {
+      resultDiv.innerText = "Final result: You Win!";
       playerWinCount = 0;
       computerWinCount = 0;
-    }
-    else if(computerWinCount === 5){
-      resultDiv.innerText = 'Final result: You Lose!';
+    } else if (computerWinCount === 5) {
+      resultDiv.innerText = "Final result: You Lose!";
       computerWinCount = 0;
       playerWinCount = 0;
     }
   });
 });
 // Define a function for playing one round
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection) {
   // Make sure the playerSelection parameter is case-insensitive
   playerSelection = playerSelection.toLowerCase();
-  console.log(playerSelection)
+  console.log(playerSelection);
   // Make a condition to check if the player wins or loses against the computer
-  switch(playerSelection){
+  switch (playerSelection) {
     // If the selection is rock
-    case 'rock':
-      if(computerSelection === 'paper'){
+    case "rock":
+      if (computerSelection === "paper") {
         return "You Lose! Paper beats Rock";
-      } 
-      else if(computerSelection === 'scissors'){
+      } else if (computerSelection === "scissors") {
         return "You Win! Rock beats Scissors";
-      }else{
+      } else {
         return "Draw";
       }
-    // If the selection is paper  
-    case 'paper':
-      if(computerSelection === 'rock'){
+    // If the selection is paper
+    case "paper":
+      if (computerSelection === "rock") {
         return "You Win! Paper beats Rock";
-      } 
-      else if(computerSelection === 'scissors'){
+      } else if (computerSelection === "scissors") {
         return "You Lose! Scissors beats Paper";
-      }else{
+      } else {
         return "Draw";
       }
-    // If the selection is scissors  
-    case 'scissors':
-      if(computerSelection === 'rock'){
+    // If the selection is scissors
+    case "scissors":
+      if (computerSelection === "rock") {
         return "You Lose! Rock beats Scissors";
-      } 
-      else if(computerSelection === 'paper'){
+      } else if (computerSelection === "paper") {
         return "You Win! Scissors beats Paper";
-      }else{
+      } else {
         return "Draw";
       }
-    // If the selection is not defined 
+    // If the selection is not defined
     default:
-      return "You must enter paper, rock or scissors"
+      return "You must enter paper, rock or scissors";
   }
 }
 
 // Define a function to play the game
-function playGame(e){
-  
+function playGame(e) {
   let result = playRound(e.target.textContent, getComputerChoice());
-  
+
   // If the player wins increase his win count
-  if(result.includes("You Win")){
+  if (result.includes("You Win")) {
     playerWinCount++;
   }
   // If the computer wins increase its win count
-  else if(result.includes("You Lose")){
+  else if (result.includes("You Lose")) {
     computerWinCount++;
-  }  
+  }
 
   resultDiv.innerText = `${result}
   Player win count: ${playerWinCount}
   Computer win count: ${computerWinCount}`;
-};
+}
